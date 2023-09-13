71615b69003493bbf8561a6c9c02d6e9dad71490
Bruno <brunolb.lafont@gmail.com>
2023-09-13 12:48:56 +0200

    add rest api cogip

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, request, jsonify
+from functools import wraps
+
+from flask import Flask, request, jsonify, make_response
 import os
 import psycopg2
 from dotenv import load_dotenv
+import jwt
+import datetime
+
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-url = os.getenv("DATABASE_URL")
-connection = conn = psycopg2.connect(
-    "host='localhost' "
-    "port='5432' "
-    "dbname='cogip' "
-    "user='postgres' "
-    "password='Pianos123.'"
+db_host = os.getenv('DATABASE_HOST')
+db_port = os.getenv('DATABASE_PORT')
+db_name = os.getenv('DATABASE_NAME')
+db_user = os.getenv('DATABASE_USER')
+db_password = os.getenv('DATABASE_PASSWORD')
+
+connection = psycopg2.connect(
+    f"host='{db_host}' "
+    f"port='{db_port}' "
+    f"dbname='{db_name}' "
+    f"user='{db_user}' "
+    f"password='{db_password}'"
 )
 
 CREATE_COMPANY_TABLE = (
@@ -32,7 +43,7 @@ INSERT_COMPANIES = (
 )
 
 CREATE_USER_TABLE = (
-    "CREATE TABLE IF NOT EXISTS user ("
+    "CREATE TABLE IF NOT EXISTS users ("
     "id SERIAL PRIMARY KEY,"
     "username VARCHAR(255) NOT NULL,"
     "password VARCHAR(255) NOT NULL,"
@@ -40,15 +51,48 @@ CREATE_USER_TABLE = (
     "role IN ('admin', 'accountant', 'intern')"
     ")"
     ");"
-)   
+)
 
 INSERT_USERS = (
-    "INSERT INTO user "
+    "INSERT INTO users"
     "(username, password, role) "
     "VALUES (%s, %s, %s) RETURNING id;"
 )
 
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 403
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired'}), 403
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Token is invalid'}), 403
+
+        return f(*args, **kwargs)
+
+    return decorated  # Return the inner decorated function as a view function
+
+@app.route("/api/login")
+def login():
+    auth = request.authorization
+
+    if auth and auth.username == 'your_username' and auth.password == 'your_password':
+        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                           app.config['SECRET_KEY'], algorithm='HS256')
+
+        return jsonify({'token': token})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
 @app.route('/api/company', methods=['POST'])
+@token_required
 def create_company():
     data = request.get_json()
     name = data["name"]
@@ -62,7 +106,9 @@ def create_company():
             company_id = cursor.fetchone()[0]
     return jsonify({"id": company_id, "message": f"Company {name} created"}), 201
 
+
 @app.route('/api/user', methods=['POST'])
+@token_required
 def create_user():
     data = request.get_json()
     username = data["username"]
@@ -74,6 +120,7 @@ def create_user():
             cursor.execute(INSERT_USERS, (username, password, role))
             user_id = cursor.fetchone()[0]
     return jsonify({"id": user_id, "message": f"User {username} created"}), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
